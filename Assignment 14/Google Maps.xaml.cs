f2d6b9cec8dbfdbd50e455fb7c5291408ab06535
Janice Muriuki <shikumuriuki512@gmail.com>
2019-04-11 13:22:10 +0300

    hiiiiiiiiiiiiiiiiiii

---
+++ Assignment 14/Google Maps.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Assignment_14
+{
+    /// <summary>
+    /// Interaction logic for Google_Maps.xaml
+    /// </summary>
+    public partial class Google_Maps : Window
+    {
+        public Google_Maps()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchClick(object sender, RoutedEventArgs e)
+        {
+            string county = txt_county.Text;
+            string livelocation = txt_livelocation.Text;
+            string hospital = txt_hospital.Text;
+
+            try
+            {
+                StringBuilder queryaddress = new StringBuilder();
+                queryaddress.Append("http://maps.google.com/maps?q=");
+
+                if(county != string.Empty)
+                {
+                    queryaddress.Append(county + "," + "+");
+                }
+                if (livelocation != string.Empty)
+                {
+                    queryaddress.Append(livelocation + "," + "+");
+                }
+                if (hospital != string.Empty)
+                {
+                    queryaddress.Append(hospital + "," + "+");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+    }
+}
---
+++ Assignment 14/Window1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Assignment_14
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You have successfully logged into your myAmbulance account! :)", 
+                "Logged In", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
